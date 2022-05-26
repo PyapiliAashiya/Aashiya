fea97425b779e0c835e2036ccd9ce0def283c036
unknown <pyapiliaashiya340@gmail.com>
2022-05-26 03:27:30 +0000

    mysql

---
+++ DockerMysql/e-cart/src/main/java/com/demo/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.bean.Order;
+import com.demo.service.OrderService;
+
+@RestController
+public class OrderController {
+
+	@Autowired
+	OrderService orderService;
+
+	@GetMapping(value="/order/id")
+	private Order getOrder(@PathVariable("id")long id) {
+		return orderService.getOrderById(id);
+		
+	}
+}
+
---
+++ DockerMysql/e-cart/src/main/java/com/demo/serviceimpl/MyOrderService.java
@@ -0,0 +1,23 @@
+package com.demo.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.bean.Order;
+import com.demo.repo.MyRepo;
+import com.demo.service.OrderService;
+
+@Service
+public class MyOrderService implements OrderService {
+
+	@Autowired
+	MyRepo myRepo;
+
+	@Override
+	public Order getOrderById(long id) {
+		
+		return myRepo.findById(id).get();
+	}
+
+
+	}
